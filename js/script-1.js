a151c8750e1d6b2eb9f70354021f09a16b555c83
Alfat1989 <gilmalf89@gmail.com>
2022-01-04 20:59:55 +0300

    создаёт файл js

---
+++ js/script-1.js
@@ -0,0 +1,31 @@
+// const a = "Hello";
+// console.log(a);
+
+// const b = "World";
+// console.log(b);
+
+// const userNumb = Number(prompt("Enter your age"));
+// console.log(userNumb);
+// console.log(typeof userNumb);
+
+// const elementWidth = Number(parseInt('50px'));
+// console.log(elementWidth);
+
+
+// const elementHeight = Number(parseFloat('128.55px'));
+// console.log(elementHeight);
+
+// const fixNum = Number(parseFloat(128.53459));
+// console.log(fixNum);
+// console.log(Number(fixNum.toFixed(2)));
+
+
+// console.log(Number('Hello'));
+// console.log(Number(''))
+
+
+const max = 60;
+const min = 15;
+const result = Math.round(Math.random() * (max - min) + min);
+
+console.log(result);
