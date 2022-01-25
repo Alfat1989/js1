ab168f73fc7106b3d14b00efae7fb21067287652
Alfat1989 <gilmalf89@gmail.com>
2022-01-25 15:28:56 +0300

    пример задачи

--- js/module-3-destructurisation.js
+++ js/module-3-destructurisation.js
@@ -3,12 +3,36 @@
 // console.log(Math.max(...[14, -4, 25, 8, 11]))
 
 
-function multiply(firstNumber, secondNumber, ...otherArgs) {
-    console.log(firstNumber); 
-  console.log(secondNumber); 
-  console.log(otherArgs);
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+//     console.log(firstNumber);
+//   console.log(secondNumber);
+//   console.log(otherArgs);
+// }
+
+// // multiply(1, 2);
+// // multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+
+
+function multiplyMax(number) {
+  let total = 0;
+  for (let i = 0; i <= number; i += 1){
+    total += i;
+  }
+  return total; 
+}
+console.log(multiplyMax(8))
+
+
+function minNumber(array) {
+  let minNum=array[0]
+  for (const arr of array) {
+    if (minNum > arr) {
+      minNum = arr;
+    }
+  }
+  return Math.round(minNum);
 }
 
-// multiply(1, 2);
-// multiply(1, 2, 3);
-multiply(1, 2, 3, 4);
+console.log(minNumber([77,45,34,7.7,11,8,12]))
