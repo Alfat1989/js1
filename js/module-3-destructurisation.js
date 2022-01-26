9135d933770cd73730c84cb38522a2aedf9dcad7
Alfat1989 <gilmalf89@gmail.com>
2022-01-26 23:07:15 +0300

    Пример деструктуризации

--- js/module-3-destructurisation.js
+++ js/module-3-destructurisation.js
@@ -38,14 +38,37 @@
 // console.log(minNumber([77,45,34,7.7,11,8,12]))
 
 
-function arrayOfNumbers(min, max) {
-  const newArray = [];
-  for (let i = min; i <= max; i += 1){
+// function arrayOfNumbers(min, max) {
+//   const newArray = [];
+//   for (let i = min; i <= max; i += 1){
     
-      newArray.push(i);
+//       newArray.push(i);
     
-  }
-  return newArray;
-}
+//   }
+//   return newArray;
+// }
 
-console.log(arrayOfNumbers(1,3))
+// console.log(arrayOfNumbers(1,3))
+
+
+const user = {
+  name: "Jacques Gluke",
+  tag: "jgluke",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const {
+  name,
+  tag,
+  stats: { followers, views: userViews, likes: userLikes = 0 },
+} = user;
+
+console.log(name); // Jacques Gluke
+console.log(tag); // jgluke
+console.log(followers); // 5603
+console.log(userViews); // 4827
+console.log(userLikes);
