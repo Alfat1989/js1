1d834d7eb55cb653136d267005adf7b2434b8a32
Alfat1989 <gilmalf89@gmail.com>
2022-01-31 22:59:26 +0300

    колбэки

--- js/module-4-callback.js
+++ js/module-4-callback.js
@@ -41,4 +41,89 @@
 
 // processCall("Манго");
 
-console.log(Math.random()>0.5)
+// function greet(name) {
+//   console.log(`Добро пожаловать ${name}.`);
+// }
+
+// // Функция высшего порядка
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest("Манго", greet);
+
+
+// function repeatLog(n) {
+//   for (let i = 0; i < n; i += 1) {
+//     console.log(i);
+//   }
+// }
+
+// repeatLog(5);
+
+// const numbers = [];
+// for (let i = 5; i <= 25; i += 1){
+//     if (i % 5 === 0) {
+//         numbers.push(i)
+//     }
+// }
+
+// console.log(numbers);
+
+// numbers.forEach(function (number, index) {
+//     console.log(`Индекс ${index}, значение ${number}`)
+// });
+
+
+
+
+///////////////////////////////////////////////////////
+//////////////////коллбэки функций/////////////////////
+
+// const doMath = function (a, b, callback/*переменная для функции add*/) {
+//     const result = callback(a, b/*передали параметры для функции add*/);
+    
+//     console.log(result); /*выдача результата*/ 
+// }
+
+// const add = function (x, y/*приняли параметры то callback (a,b)*/) {
+//     return x + y; 
+// }
+
+// const sub = function (x, y) {
+//     return x - y;
+// }
+
+// doMath(4, 5, add/*ссылка на функцию*/);
+// doMath(20, 18, add)
+// doMath(20, 18, sub)
+
+// ////аналогичная задача//анонимные функции////////
+
+// doMath(6, 7, function (x, y) {
+//     return x * y;
+// });
+
+/////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+
+
+
+
+///////////////////////////////////ЗАМЫКАНИЯ/////////////////////////////////
+
+
+const fnA = function (parametr) {
+    const innerVariablr = "Значение внутренней переменной";
+
+    const innerFunction = function () {
+        console.log("Это вызов innerFunction");
+    };
+    return innerFunction;
+};
+
+const fnB = fnA();
+fnB()
+console.log(fnB)
