fc0c2c51254344befe14403ab2fcf274128bacf9
Alfat1989 <gilmalf89@gmail.com>
2022-02-04 20:44:29 +0300

    создана папка для метода forEach

---
+++ js/forEach.js
@@ -0,0 +1,26 @@
+const filter = (array, test) => {
+    const filteredArray = [];
+    for (let el of array) {
+        const passed = test(el);
+        if (passed) {
+            filteredArray.push(el)
+        }
+    }
+    return filteredArray;
+}
+
+
+
+const fruits=[
+{ name: "Apple", price: 50 },
+{ name: "Grape", price: 60 },
+{ name: "Strawberry", price: 110 },
+];
+
+
+const callback = fruit => {
+    return fruit.price >= 60;
+}
+
+console.log(filter(fruits, callback))
+
