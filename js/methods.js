107786444ef6f3f8f86bdd23751c6942fb893da5
Alfat1989 <gilmalf89@gmail.com>
2022-02-07 21:59:10 +0300

    решение задач по методам

--- js/methods.js
+++ js/methods.js
@@ -69,6 +69,154 @@
 
 
 
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//     age: 37
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//     age: 34
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//     age: 24
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//     age: 21
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//     age: 27
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//     age: 38
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//     age: 39
+//   }
+// ]
+
+// const getUsersWithEyeColor = (users, color) =>
+//  users.filter(user=>user.eyeColor===color)
+
+// // console.log(getUsersWithEyeColor(users, "brown"));
+
+
+// const getUsersWithAge = (users, minAge, maxAge) =>
+// users.filter(({ age }) => age > minAge && age < maxAge);
+    
+
+// // console.log(getUsersWithAge(users, 25, 60));
+
+
+// const getUsersWithFriend = (users, friendName) =>
+// users.filter(user => user.friends.includes(friendName))
+
+// console.log(getUsersWithFriend(users, "Goldie Gentry"));
+
+
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+const newTags = tweets => tweets.reduce((acc, elm) => { acc.push(...elm.tags); return acc.filter((el,ind,arr)=>arr.indexOf(el)===ind) },[])
+
+
+// console.log(newTags(tweets))
+
+
+// console.log()
+
+const scores = [27, 2, 41, 4, 7, 3, 75]
+
+const newScores = [...scores].sort((a,b)=>a-b)
+
+// console.log(newScores)
+
+
+
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+
+// const MIN_BOOK_RATING = 8;
+
+// const names = books.filter(book => book.rating > MIN_BOOK_RATING).map(elm => elm.author).sort((a, b) => a.localeCompare(b));
+
+// console.log(names)
+
+
 const users = [
   {
     name: "Moore Hensley",
@@ -77,8 +225,7 @@ const users = [
     friends: ["Sharron Pace"],
     isActive: false,
     balance: 2811,
-    gender: "male",
-    age: 37
+    gender: "male"
   },
   {
     name: "Sharlene Bush",
@@ -87,8 +234,7 @@ const users = [
     friends: ["Briana Decker", "Sharron Pace"],
     isActive: true,
     balance: 3821,
-    gender: "female",
-    age: 34
+    gender: "female"
   },
   {
     name: "Ross Vazquez",
@@ -97,8 +243,7 @@ const users = [
     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
     isActive: false,
     balance: 3793,
-    gender: "male",
-    age: 24
+    gender: "male"
   },
   {
     name: "Elma Head",
@@ -107,28 +252,25 @@ const users = [
     friends: ["Goldie Gentry", "Aisha Tran"],
     isActive: true,
     balance: 2278,
-    gender: "female",
-    age: 21
+    gender: "female"
   },
   {
     name: "Carey Barr",
     email: "careybarr@nurali.com",
     eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
     isActive: true,
     balance: 3951,
-    gender: "male",
-    age: 27
+    gender: "male"
   },
   {
     name: "Blackburn Dotson",
     email: "blackburndotson@furnigeer.com",
     eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
     isActive: false,
     balance: 1498,
-    gender: "male",
-    age: 38
+    gender: "male"
   },
   {
     name: "Sheree Anthony",
@@ -137,25 +279,22 @@ const users = [
     friends: ["Goldie Gentry", "Briana Decker"],
     isActive: true,
     balance: 2764,
-    gender: "female",
-    age: 39
+    gender: "female"
   }
-]
+];
 
-const getUsersWithEyeColor = (users, color) =>
- users.filter(user=>user.eyeColor===color)
+// const getNamesSortedByFriendCount = users => [...users].sort((more, few) => more.friends.length - few.friends.length).map(user=>user.name)
 
-// console.log(getUsersWithEyeColor(users, "brown"));
+// console.log(getNamesSortedByFriendCount(users))
 
+// const getSortedFriends = users => users.flatMap(user => user.friends)
+//   .filter((user, ind, arr) => arr.indexOf(user) === ind)
+//   .sort((a, b) => a.localeCompare(b));
 
-const getUsersWithAge = (users, minAge, maxAge) =>
-users.filter(({ age }) => age > minAge && age < maxAge);
-    
-
-// console.log(getUsersWithAge(users, 25, 60));
+// console.log(getSortedFriends(users))
 
 
-const getUsersWithFriend = (users, friendName) =>
-users.filter(user => user.friends.includes(friendName))
-
-console.log(getUsersWithFriend(users, "Goldie Gentry"));
+const getTotalBalanceByGender = (users, gender) => users.filter(user=>user.gender===gender).reduce((total,user)=>
+  total += user.balance, 0)
+  
+  console.log(getTotalBalanceByGender(users,"male"))
