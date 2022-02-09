34e21cbfd6cf2d8a22a3d03b98595125c2da08b2
Alfat1989 <gilmalf89@gmail.com>
2022-02-09 23:02:16 +0300

    this  в коллбэк функциях

--- js/05-this.js
+++ js/05-this.js
@@ -1,33 +1,53 @@
-const showTag = function () {
+// const showTag = function () {
     
-    console.log('Показывает объект', this)
-    console.log(`Показывает тег этого обьекта, "${this.tag}"`)
-}
+//     console.log('Показывает объект', this)
+//     console.log(`Показывает тег этого обьекта, "${this.tag}"`)
+// }
 
 
-const user = {
-    tag: "Mango",
-    city: "Tokyo",
-}
+// const user = {
+//     tag: "Mango",
+//     city: "Tokyo",
+// }
 
-const player = {
-    tag: "Poly",
-    city: "Paris",
+// const player = {
+//     tag: "Poly",
+//     city: "Paris",
     
+// }
+
+// user.showUserTage = showTag;
+
+// player.showPlayerTag = showTag;
+
+
+// user.showUserTage()
+
+// player.showPlayerTag()
+
+// const xyz = user.showUserTage;
+
+// console.log(xyz)
+
+const customer = {
+  firstName: "Jacob",
+  lastName: "Mercer",
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+function makeMessage(callback) {
+  // callback() это вызов метода getFullName без объекта
+  console.log(`Обрабатываем заявку от ${callback()}.`);
 }
 
-user.showUserTage = showTag;
 
-player.showPlayerTag = showTag;
-
-
-user.showUserTage()
-
-player.showPlayerTag()
-
-const xyz = user.showUserTage;
-
-console.log(xyz)
+customer.getThis = customer.getFullName;
 
 
+console.log(customer.getThis())
+
+
+// makeMessage(customer.getFullName.bind(customer));
 
