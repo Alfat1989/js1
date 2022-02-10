5ec1cb8663552b0852f09b6465f0acd5c625de5e
Alfat1989 <gilmalf89@gmail.com>
2022-02-10 22:30:20 +0300

    прототипы

--- js/05-this.js
+++ js/05-this.js
@@ -29,24 +29,107 @@
 
 // console.log(xyz)
 
-const customer = {
-  firstName: "Jacob",
-  lastName: "Mercer",
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
+// const customer = {
+//   firstName: "Jacob",
+//   lastName: "Mercer",
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
 
-function makeMessage(callback) {
-  // callback() это вызов метода getFullName без объекта
-  console.log(`Обрабатываем заявку от ${callback()}.`);
+// // function makeMessage(callback) {
+// //   // callback() это вызов метода getFullName без объекта
+// //   console.log(`Обрабатываем заявку от ${callback()}.`);
+// // }
+
+
+// customer.getThis = customer.getFullName;
+
+
+// console.log(customer.getThis())
+
+// const another = customer.getFullName();
+
+
+// const user = {
+//   tag: "Mango",
+//   showTag() {
+//     console.log(this);
+//     console.log(this.tag);
+//   },
+// }
+
+// // user.showTag()
+
+// const invokeAction = function (action) {
+//   console.log(action);
+  
+  
+//   action()
+// }
+
+// invokeAction(user.showTag)
+
+
+const book = {
+  title: "React for beginner",
+  
+  showTitle() {
+    console.log(this.title);
+  },
 }
 
+const showThis=function() {
+    console.log(this);
+  }
 
-customer.getThis = customer.getFullName;
+const bookSecond = {
+  title: "JS for beginner",
+  
+}
+
+bookSecond.showThis = showThis;
+ 
+bookSecond.showThis()
 
 
-console.log(customer.getThis())
+const counter = {
+  value: 0,
+  increment(value){
+    console.log("increment -> this", this);
+    this.value += value;
+    console.log(this)
+  },
+  decrement(value){
+    console.log("increment -> this", this);
+    this.value -= value;
+  }
+}
+
+const updateCounter = function (value, operation) {
+  operation(value)
+}
+
+// updateCounter(10, counter.increment());
+
+// counter.increment(10);
+
+
+function greetGuest(greeting,a,b) {
+  console.log(`${greeting}, ${this.username}.`);
+}
+
+const mango = {
+  username: "Манго",
+};
+const poly = {
+  username: "Поли",
+};
+
+greetGuest.call(mango, "Добро пожаловать" );
+ 
+
+
 
 
 // makeMessage(customer.getFullName.bind(customer));
