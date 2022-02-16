f17cd8941f251f6bbebeaa20cb10dd1e444ace67
Alfat1989 <gilmalf89@gmail.com>
2022-02-16 21:54:49 +0300

    практика с замыканиями

--- js/module-4-callback.js
+++ js/module-4-callback.js
@@ -129,4 +129,71 @@
 // console.log(fnB)
 
 
-/////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////// З А М Ы К А Н И Я ///////////////////////////////////
+
+const closure = function () {
+    const makeMaster = function (name) {
+    const makeDetal = function (detal, detal2) {
+        console.log(`${name} ремонтирует ${detal} и ${detal2}`)
+    }
+    return makeDetal;
+}
+
+const masterPetr = makeMaster('Пётр');  // передаем в пременную результат вызова функции с параметром
+const masterAlex = makeMaster("Алексей"); // передаем в пременную результат вызова функции с параметром
+
+masterPetr("мотор", "кпп") // вызвали пременную с именем Пётр и предпли ей значение для другой функции
+masterAlex("кузов", "дно")
+masterAlex("фары", "радио")
+masterAlex("кпп", "редкутор")
+
+
+const rounder = function (places) {
+    return function (number) {
+        return Number(number.toFixed(places))
+    }
+}
+
+
+const rounder2 = rounder(2)
+console.log(rounder2(44.3457332))
+
+
+const multiply = function (mult) {
+    return function (number) {
+        return number*mult
+    }
+}
+
+
+const multiply2 = multiply(2);
+const multiply3 = multiply(3);
+const multiply4 = multiply(4);
+
+console.log(multiply2(33));
+console.log(multiply3(33));
+console.log(multiply4(33));
+    
+}
+
+// console.log(closure()) 
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const user = {
+    fullName: "Mango",
+    showName() {
+        console.log("this: ", this);
+        console.log("this.fillName: ", this.fullName);
+
+    },
+};
+
+user.showName.call(user)
