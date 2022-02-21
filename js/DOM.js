2b64816f0bb88de2598f77b1e0b5c868a09ce99d
Alfat1989 <gilmalf89@gmail.com>
2022-02-21 22:52:39 +0300

    практика с innerHTML

--- js/DOM.js
+++ js/DOM.js
@@ -1,76 +1,51 @@
-// const listWithId = document.querySelector(".menu");
-// listWithId.style.textTransform = "uppercase";
-// listWithId.style.fontSize = '24px';
-// console.log(listWithId);
-
-// const listWithClass = document.querySelector('.menu');
-// console.log(listWithClass);
-
-// const menuItemsByTagName = document.querySelectorAll("li");
-// console.log(menuItemsByTagName);
-
-// const menuItemsByClass = document.querySelectorAll(".menu-item");
-// console.log(menuItemsByClass);
-
-// const firstMenuItem = document.querySelector(".menu-item");
-// firstMenuItem.style.color = 'tomato';
-// console.log(firstMenuItem);
-
-// const message = document.querySelector("#message");
-// console.log(message.value);
-
-// const activeLink = document.querySelector(".btn.active");
-// console.log(activeLink.href);
-
-// const image = document.querySelector(".image");
-// console.log(image.src); // https://placeimg.com/640/480/animals
-// image.src = 'https://placeimg.com/640/480/tech';
 
 
-// const saveBtn = document.querySelector('.editor button[data-action="save"]');
-// const closeBtn = document.querySelector('.editor button[data-action="close"]');
+const btnEl = document.querySelector(".btn")
+btnEl.style.backgroundColor="pink"
+btnEl.textContent="pink"
+btnEl.setAttribute("type", "button")
 
-// console.log(saveBtn.dataset.action); //save
-// console.log(closeBtn.dataset.action); //close
+btnEl.setAttribute("data-action", "save")
 
-// const dishes = document.querySelectorAll(".dishes > li");
-// dishes.forEach((dish) => {
-//   console.log(dish.dataset.id);
-//   console.log(dish.innerHTML);
-// });
+console.log(btnEl.dataset.action)
 
-// const article = document.querySelector(".article");
-// console.log(article.innerHTML);
-
-// const title = document.querySelector(".article .title");
-// console.log(title.innerHTML);
+console.log(btnEl.hasAttribute("type"))
 
 
-// title.innerHTML='New and <span class="accent">improved</span> title';
-
-// const text = document.querySelector(".article .text");
-// console.log(text.innerHTML);
-
-// const link = document.querySelector(".article .link");
-// console.log(link.innerHTML);
+console.log(btnEl)
+console.log(btnEl.attributes)
 
 
-const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
-const list = document.querySelector(".list");
+const newTagEl = document.createElement("h1");
+newTagEl.textContent="This is h1 tag"
+console.log(newTagEl)
 
-const markup = technologies
-    .map((technology) => `<li class="list-item">${technology}</li>`)
-    .join("");
-  
+const imageEl = document.createElement("img")
+imageEl.src = "https://avatars.mds.yandex.net/i?id=5f28b35384960743cf4aa743467a92fd-5877456-images-thumbs&n=13"
+imageEl.alt = "Cosmos"
 
-console.log(markup);
- 
-list.innerHTML = markup;
 
-const subtitleEl = document.querySelector("h2")
-console.log(subtitleEl)
-const subtitleElButton = document.addEventListener("click", subtitleEl)
 
-const htmlEl = document.querySelectorAll(".list-item")
-htmlEl.forEach(el=>el.innerHTML="hello")
-console.log(htmlEl)
+console.log(imageEl)
+
+const listEl = document.createElement("ul")
+console.log(listEl)
+
+const itemUlEl = document.createElement("li")
+itemUlEl.classList="img-main"
+console.log(itemUlEl)
+
+itemUlEl.append(imageEl)
+
+listEl.append(itemUlEl)
+
+console.log(listEl)
+
+const listUlEl = document.querySelector(".list-element")
+listUlEl.append(itemUlEl)
+console.log(listUlEl)
+
+const article = document.querySelector(".article");
+console.log(article.innerHTML);
+
+
