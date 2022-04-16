31e3e1f52e4155fc718f19a03c7c54c0fc301135
Alfat1989 <gilmalf89@gmail.com>
2022-04-16 21:58:38 +0300

    Добавляет 9 модуль для практики по промисам

---
+++ js/module-9.js
@@ -0,0 +1,71 @@
+// console.log('This is 9 modupr "promis" ');
+
+// const isSuccess = Math.round(Math.random()*(50 - 20) + 20)
+// // const isSuccess = true
+
+// console.log(isSuccess)
+
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (isSuccess>40) {
+//             resolve('yes')
+//         }
+//         else {
+//             reject('no')
+//         }
+//     }, 1000)
+// });
+
+// promise.then(value => {
+//     console.log("onResolve call inside promise.then()");
+//     console.log(value); // "Success! Value passed to resolve function"
+//   },
+//   // onReject will run third or not at all
+//   error => {
+//     console.log("onReject call inside promise.then()");
+//     console.log(error); // "Error! Error passed to reject function"
+//   }
+// );
+// promise.then(onYes).then(onSecondYes).catch(onNo).finally(onFinaly) 
+
+// function onYes() {
+//     console.log('will yesssssss')
+//     console.log(value)
+    
+// }
+
+// function onSecondYes() {
+//     console.log('will second yesssssss')
+//     console.log(value)
+    
+// }
+
+// function onNo() {
+//     console.log('Noooooooooo')
+// }
+
+// function onFinaly() {
+//     console.log('Promis end!!!!!!!!!!')
+// }
+
+
+
+
+////////////////////////////////////////////
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(5);
+  }, 2000);
+});
+
+promise.then(value => {
+    console.log('yes');
+    return value*2
+}).then(value => {
+    console.log('second yes');
+    console.log(value)
+}).catch(error => {
+    console.log(error)
+}).finally()
+
